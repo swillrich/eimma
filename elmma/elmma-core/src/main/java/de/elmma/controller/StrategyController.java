ac5fe8c9e7bdd2b6ab522e8c9e814c11997c3e11
Sven Willrich <willrich@fzi.de>
2017-11-10 08:56:35 +0100

    add comments

--- elmma/elmma-core/src/main/java/de/elmma/controller/StrategyController.java
+++ elmma/elmma-core/src/main/java/de/elmma/controller/StrategyController.java
@@ -10,9 +10,27 @@ import org.springframework.web.bind.annotation.RestController;
 import de.elmma.model.Performance;
 import de.elmma.strategies.StrategyOperator;
 
+/**
+ * Führt eine Strategie auf eine bestimmte Dauer mit einem bestimmten Invest aus
+ * und gibt das Ergebnis (JSON) zurück.
+ */
 @RestController
 @RequestMapping("/strategy")
 public class StrategyController {
+	/**
+	 * @param from
+	 *            Zeitraum von (yyyy-MM-dd)
+	 * @param to
+	 *            Zeitraum bis (yyyy-MM-dd)
+	 * @param invest
+	 *            Gesamtinvest (bspw. 1000,00)
+	 * @param strategy
+	 *            Name der Strategie (bspw. TSS)
+	 * @return
+	 * @throws ParseException
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 */
 	@RequestMapping(method = RequestMethod.GET)
 	public Performance prices(@RequestParam(value = "from", required = false) String from,
 			@RequestParam(value = "to", required = false) String to,
--- elmma/elmma-core/src/main/java/de/elmma/strategies/StrategyOperator.java
+++ elmma/elmma-core/src/main/java/de/elmma/strategies/StrategyOperator.java
@@ -5,14 +5,28 @@ import java.util.Arrays;
 import de.elmma.dbio.PriceDAO;
 import de.elmma.model.Performance;
 
+/**
+ * Sucht die angeforderte Strategie und führt sie auf eine bestimmten
+ * Kurs-Verlauf aus.
+ */
 public class StrategyOperator {
 
+	/**
+	 * Alle bisher implementierten / lauffähigen Strategien, die per
+	 * JSON-Rest-Schnittstelle aufrufbar / anwendbar sein sollen.
+	 */
 	private static Class<? extends Strategy>[] strategies = new Class[] { TSS.class };
 
+	/**
+	 * Wendet eine Strategie an
+	 * 
+	 * @param strategyName
+	 *            Name der Strategie (Name der Klasse, bspw. TSS)
+	 */
 	public static Performance apply(String strategyName, double invest, String to, String from)
 			throws InstantiationException, IllegalAccessException {
-		Strategy strategy = Arrays.stream(strategies).filter(s -> s.getSimpleName().equals(strategyName)).iterator().next()
-				.newInstance();
+		Strategy strategy = Arrays.stream(strategies).filter(s -> s.getSimpleName().equals(strategyName)).iterator()
+				.next().newInstance();
 		strategy.setInvest(invest);
 		PriceDAO.getPrices(from, to).forEach(p -> strategy.onUpdate(p));
 		return strategy.getPerformance();
