d0d18636ef779c30d3b422acff876e6277bb48fd
Friedrich Politz <schreibtisch@fpolitz.de>
2017-11-16 15:38:45 +0100

    Improved scraper

--- exchange-api/src/dax-finanzen-scraper.route.js
+++ exchange-api/src/dax-finanzen-scraper.route.js
@@ -1,13 +1,13 @@
-const puppeteer = require('puppeteer');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 const DAX_PRICE_SELECTOR = 'body > div.wrapper > div.container.mobile > div.shadow > div:nth-child(13) > div:nth-child(1) > div > div > div.row.snapshot-headline > div.col-sm-7 > div.row.quotebox > div.col-xs-5.col-sm-4.text-sm-right.text-nowrap';
 
 exports.scrapeFinanzenNetDax = async (req, res) => {
-  const BROWSER = await puppeteer.launch();
-  const page = await BROWSER.newPage();
-  await page.goto('http://www.finanzen.net/index/DAX-Realtime');
-  const bodyHandle = await page.$(DAX_PRICE_SELECTOR);
-  const html = await page.evaluate(body => body.innerHTML, bodyHandle);
-  await bodyHandle.dispose();
-  res(html);
+  const startTime = new Date();
+  const html = await axios('http://www.finanzen.net/index/DAX-Realtime');
+  const $ = cheerio.load(html.data)
+  const endTime = new Date();
+  console.log(`Execution Time: ${Math.abs(new Date() - startTime) / 1000}`);
+  res($(DAX_PRICE_SELECTOR).text());
 }
